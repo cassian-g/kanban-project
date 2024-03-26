1709a7b227d81384cabb1e7880c3411dd51bb67a
Cassian Goode <cassian.goode@gmail.com>
2024-03-26 16:35:47 -0400

    Further updates

--- spotifyReactApp/jammming/src/SearchBar.js
+++ spotifyReactApp/jammming/src/SearchBar.js
@@ -1,33 +1,30 @@
 import React, { useState } from "react";
 import SearchBarPresentation from "./SearchBarPresentation";
-import styles from "./SearchBar.module.css";
+import onSearch from "./OnSearch";
 
 function SearchBar() {
-  
+
   const [query, setQuery] = useState('');
-
-  function handleChange(event) {
-    setQuery(event.target.value)
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    const onSearch = () => {
-      console.log('Searched for', query)
-    }
-    onSearch(query);
-  }
-
+  const [results, setResults] = useState([]);
   
+  const handleChange = (event) => {
+    setQuery(event.target.value);
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const searchResults = await onSearch(query);
+    setResults(searchResults);
+  }
+
   return (
     <div>
-      <SearchBarPresentation 
+      <SearchBarPresentation
         query={query}
         handleChange={handleChange}
-        handleSubmit={handleSubmit}
-      />
+        handleSubmit={handleSubmit} />
     </div>
-  )
+  );
 }
 
 export default SearchBar;
